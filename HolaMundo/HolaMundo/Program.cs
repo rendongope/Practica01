2ef97236069a5df8a4d997b1090db1756d0e4ba4
Pedro Rendón Gómez <rendongope@gmail.com>
2017-07-15 10:24:28 -0500

    Practica01
    Se agregó Hola mundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi madafaka");
+            Console.ReadLine();
+
+        }
+    }
+}
